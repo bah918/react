03d0917193923bd29c08541de7d4be60f5a23636
pknowledge <nafiou6@yahoo.fr>
2021-02-02 15:02:36 +0100

    formulaire

--- jour4/src/Form.jsx
+++ jour4/src/Form.jsx
@@ -3,21 +3,29 @@ import React from "react";
 class Form extends React.Component {
   render() {
     return (
-
-         <div className="container">
-             <h2 className="text-center">Login</h2>
-             <p>Adresse mail</p>
-            <div class="form-floating mb-3">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-              <label for="floatingInput">Email address</label>
+          
+        
+        <form className="form-group needs-validation container-fluid">
+            <h2 className="text-center">LOGIN</h2>
+            <div className="row">
+                 <div className="col-8">
+                   <label for="floatingInput">Email address</label>
+                   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                 </div>
             </div>
-            <p>password</p>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-              <label for="floatingPassword">Password</label>
+            <div className="row">
+              <div className="col-8">
+                <label for="floatingPassword">Password</label>
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+              </div>
             </div>
-         </div>
-
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button class="btn btn-primary" type="button">Button</button>
+            
+         </form>
        );
   }
 }
